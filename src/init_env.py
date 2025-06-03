8f6a79161bed9407eb89f8dd2b534edc64984765
miguelbalcazarr <miguelbalcazarr>
2025-06-03 06:29:23 +0000

    estructura

---
+++ src/init_env.py
@@ -0,0 +1,12 @@
+import sys
+from env_utils import get_env, get_scope
+from databricks.sdk.runtime import *
+
+env = get_env()
+scope = get_scope()
+
+container = dbutils.secrets.get(scope, "secret-env-container")
+storage_account = dbutils.secrets.get(scope, "secret-env-storage-account")
+environment = dbutils.secrets.get(scope, "secret-env-environment")
+path_base = f"abfss://{container}@{storage_account}.dfs.core.windows.net/{environment}"
+catalog = "desarrollo"
