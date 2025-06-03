7e60462f97f63cf90f8f454362a54ee3a7a142ae
miguelbalcazarr <miguelbalcazarr>
2025-06-03 20:52:02 +0000

    estructura datalakehouse databricks

--- src/env_utils.py
+++ src/env_utils.py
@@ -1,8 +1,14 @@
 import json
 import os
+from pathlib import Path
 
-def get_env():
-    env_path = "/Workspace/Users/miguel.balcazar@kyndryl.com/databricks-estructura/config/env.json"
+def get_env_path() -> str:
+    notebook_path = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+    base_path = "/".join(notebook_path.split("/")[:3])  # /Workspace/Users/<user>
+    return f"{base_path}/databricks-estructura/config/env.json"
+
+def get_env() -> dict:
+    env_path = get_env_path()
     with open(env_path) as f:
         return json.load(f)
 
