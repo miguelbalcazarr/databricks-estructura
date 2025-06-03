29253d6bb23a7a8de27b01a79a418597617a24cb
miguelbalcazarr <miguelbalcazarr>
2025-06-03 20:49:39 +0000

    estructura datalakehouse databricks

--- src/env_utils.py
+++ src/env_utils.py
@@ -1,24 +1,11 @@
+import json
 import os
 
-def get_env(default: str = "dev") -> str:
-    try:
-        return dbutils.widgets.get("env")
-    except:
-        return os.getenv("ENV", default)
+def get_env():
+    env_path = "/Workspace/Users/miguel.balcazar@kyndryl.com/databricks-estructura/config/env.json"
+    with open(env_path) as f:
+        return json.load(f)
 
-
-def get_scope(service: str = "databricks") -> str:
+def get_scope(key: str) -> str:
     env = get_env()
-    scope_map = {
-        "databricks": {
-            "dev": "scope-dev",
-            "qa": "scope-qa",
-            "prod": "scope-prd"
-        },
-        "sql": {
-            "dev": "sql-scope-dev",
-            "qa": "sql-scope-qa",
-            "prod": "sql-scope-prod"
-        },
-    }
-    return scope_map[service][env]
+    return env["scopes"][key]
