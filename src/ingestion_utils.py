28eb0d763adc9bb2f012bc655284b84a4939d9c6
miguelbalcazarr <miguelbalcazarr>
2025-06-03 20:30:56 +0000

    estructura datalakehouse

---
+++ src/ingestion_utils.py
@@ -0,0 +1,8 @@
+def primera_funcion() -> str:
+    return "primera funcion"
+
+def segunda_funcion() -> str:
+    return "segunda funcion"
+
+def tercera_funcion() -> str:
+    return "tercera funcion"
