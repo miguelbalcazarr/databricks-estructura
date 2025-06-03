ae65da1048ed20b230b5d8598b81b9e3275caf4b
miguelbalcazarr <miguelbalcazarr>
2025-06-03 14:42:35 +0000

    estructura

--- src/ingestion_utils.py
+++
@@ -1,8 +0,0 @@
-def primera_funcion() -> str:
-    return "primera funcion"
-
-def segunda_funcion() -> str:
-    return "segunda funcion"
-
-def tercera_funcion() -> str:
-    return "tercera funcion"
