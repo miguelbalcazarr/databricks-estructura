6b2b4686c2b24be8111eb55e36404024a35bcf41
miguelbalcazarr <miguelbalcazarr>
2025-06-04 05:49:23 +0000

    restaturacion de commit 16b8277c26f985d2372ab35b3fdae970d685fcd1

--- src/env_utils.py
+++ src/env_utils.py
@@ -1,24 +1,20 @@
-import os
+from databricks.sdk.runtime import dbutils
+import os, json
 
-def get_env(default: str = "dev") -> str:
-    try:
-        return dbutils.widgets.get("env")
-    except:
-        return os.getenv("ENV", default)
+def get_env_path() -> str:
+    notebook_path = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+    
+    if notebook_path.startswith("/Repos/"):
+        base_path = "/".join(notebook_path.split("/")[:4])  # /Repos/<user>/<repo>
+    else:
+        base_path = "/".join(notebook_path.split("/")[:3])  # /Workspace/Users/<user>
 
+    return f"{base_path}/config/env.json"
 
-def get_scope(service: str = "databricks") -> str:
-    env = get_env()
-    scope_map = {
-        "databricks": {
-            "dev": "scope-dev",
-            "qa": "scope-qa",
-            "prod": "scope-prd"
-        },
-        "sql": {
-            "dev": "sql-scope-dev",
-            "qa": "sql-scope-qa",
-            "prod": "sql-scope-prod"
-        },
-    }
-    return scope_map[service][env]
+def get_env() -> dict:
+    env_path = get_env_path()
+    with open(env_path) as f:
+        return json.load(f)
+
+def get_scope(key: str) -> str:
+    return get_env()["scopes"][key]
